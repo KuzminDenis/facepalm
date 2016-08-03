18cba5530ccb830ce5e60924275a40e9680c39d5
kuzmindenis <kuzmindenis94@gmail.com>
2016-08-03 15:58:14 +0300

    fix media path

--- main/settings.py
+++ main/settings.py
@@ -122,5 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT='/Django/facepalm/media/'
+#MEDIA_ROOT='/Django/facepalm/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
